dbaf24a5da1ad1b2e6877eca541b79d7435f6272
vvparkhomenko <vyacheslav.parkhomenko@gmail.com>
2019-10-10 16:13:15 +0300

    Modified templates to display posts from database.

--- infpoolsite/views.py
+++ infpoolsite/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    return render(request, 'infpoolsite/post_list.html', {})
+    posts = Post.objects.all().order_by('title')
+    return render(request, 'infpoolsite/post_list.html', {'posts':posts})
